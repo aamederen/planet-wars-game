1fe4f8b1fd30591404b2d01783fb1d03a67b635d
Eren Basak <aamederen@gmail.com>
2024-01-28 00:48:17 +0300

    better upgrades

--- scripts/upgrade_pack.gd
+++ scripts/upgrade_pack.gd
@@ -2,7 +2,10 @@ extends Spatial
 
 var type
 var brain:BigBrain
+onready var rotation_vector:Vector3 = Vector3(randf(), randf(), randf()).normalized()
 
 func _on_CollisionArea_body_entered(body):
 	if brain:
 		brain.upgrade_pack_hit_something(self, body)
+func _physics_process(delta):
+	rotate(rotation_vector, delta)
